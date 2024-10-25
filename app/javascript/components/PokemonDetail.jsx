f22fcb5f44124ec97fca5271510058f0458e81e1
Sam <Samuel3877@hotmail.co.uk>
2024-10-25 13:26:14 +0100

    PokemonDetail Styling
    Styled the PokemonDetail component and fixed any div discrepancies

--- app/javascript/components/PokemonDetail.jsx
+++ app/javascript/components/PokemonDetail.jsx
@@ -3,31 +3,39 @@ import { Link } from "react-router-dom";
 
 const PokemonDetail = () => {
     return (
-        <div>
-            <div>
-                <img />
-                <div>
-                    <h1>Pokemon Name</h1>
-                </div>
-                <div>
-                    <div>
-                        <div>
-                            <ul>
-                                <h5>Stats</h5>
-                            </ul>
-                        </div>
-                        <div>
-                            <h5>Description</h5>
-                            <div>Content</div>
-                            <div>
-                                <button>Delete Pokemon</button>
-                            </div>
-                        </div>
-                        <Link>
-                            List all Pokemon
-                        </Link>
+        <div className="">
+            <div className="hero position-relative d-flex align-items-center justify-content-center">
+                <img
+                     className="img-fluid position-absolute"
+                />
+                <div className="overlay bg-dark position-absolute" />
+                <h1 className="display-4 position-relative text-white">
+                    Pokemon Name
+                </h1>
+            </div>
+            <div className="container py-5">
+                <div className="row">
+                    <div className="col-sm-12 col-lg-3">
+                        <ul className="list-group">
+                            <h5 className="mb-2">Stats</h5>
+                        </ul>
+                    </div>
+                    <div className="col-sm-12 col-lg-7">
+                        <h5 className="mb-2">Description</h5>
+                        <div>Content</div>
+                    </div>
+                    <div className="col-sm-12 col-lg-2">
+                        <button
+                            type="button"
+                            className="btn btn-danger"
+                        >
+                            Delete Pokemon
+                        </button>
                     </div>
                 </div>
+                <Link to="/pokemon" className="btn btn-link">
+                    List all Pokemon
+                </Link>
             </div>
         </div>
     );
