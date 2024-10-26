de28d18e2324fd9e58d2327324edc35a8c44323a
Sam <Samuel3877@hotmail.co.uk>
2024-10-26 20:28:06 +0100

    PokemonDetail Component Shows Pokemon Details
    Updated PokemonDetail to show data correctly
    Updated PokemonDetail with some of my own styling and an extra image in the header
    Created a custom scss file
    Updated Bootstrap scss file to include the custom file

--- app/javascript/components/PokemonDetail.jsx
+++ app/javascript/components/PokemonDetail.jsx
@@ -3,7 +3,7 @@ import { Link, useParams } from "react-router-dom";
 
 const PokemonDetail = () => {
     const params = useParams();
-    const [pokemon, setPokemon] = useState();
+    const [pokemon, setPokemon] = useState({});
 
     useEffect(() => {
         const url = `/api/v1/show/${params.id}`;
@@ -22,18 +22,24 @@ const PokemonDetail = () => {
         <div className="">
             <div className="hero position-relative d-flex align-items-center justify-content-center">
                 <img
-                    src={pokemon.image}
-                    alt={`${pokemon.name} image`}
-                     className="img-fluid position-absolute"
+                src={pokemon.image}
+                alt={`${pokemon.name} image`}
+                className="img-fluid position-absolute"
                 />
                 <div className="overlay bg-dark position-absolute" />
-                <h1 className="display-4 position-relative text-white">
+                <h1 className="display-3 position-relative text-white">
                     {pokemon.name}
                 </h1>
+                <img
+                    src={pokemon.image}
+                    alt={`${pokemon.name} image`}
+                    className="position-relative ms-50"
+                    style={{ width: '200px', height: 'auto', borderRadius: '15px', marginLeft: '120px' }}
+                />
             </div>
             <div className="container py-5">
                 <div className="row">
-                    <div className="col-sm-12 col-lg-3">
+                    <div className="col-sm-12 col-lg-2 border-end">
                         <ul className="list-group">
                             <h5 className="mb-2">Stats</h5>
                             <li>HP: {pokemon.health}</li>
@@ -44,9 +50,9 @@ const PokemonDetail = () => {
                     </div>
                     <div className="col-sm-12 col-lg-7">
                         <h5 className="mb-2">Description</h5>
-                        <div>Content</div>
+                        {pokemon.description}
                     </div>
-                    <div className="col-sm-12 col-lg-2">
+                    <div className="col-sm-12 col-lg-3 text-center">
                         <button
                             type="button"
                             className="btn btn-danger"
@@ -55,7 +61,7 @@ const PokemonDetail = () => {
                         </button>
                     </div>
                 </div>
-                <Link to="/pokemon" className="btn btn-link">
+                <Link to="/pokemon" className="btn btn-link ps-0">
                     List all Pokemon
                 </Link>
             </div>
